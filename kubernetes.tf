75b73349c715cbe627ed61b461f19f03207c4dc3
modovar <modhaffer.raihane@hotmail.com>
2025-02-24 12:07:06 +0100

    updates kube manifest

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,26 +1,26 @@
-# resource "kubectl_manifest" "cluster_issuer" {
-#     yaml_body = file("./kubernetes/manifests/certificates/clusterissuer.yaml")
-#     depends_on = [helm_release.cert_manager]
-# }
+resource "kubectl_manifest" "cluster_issuer" {
+    yaml_body = file("./kubernetes/manifests/certificates/clusterissuer.yaml")
+    depends_on = [helm_release.cert_manager]
+}
 
 
-# resource "kubectl_manifest" "certificate_grafana" {
-#     yaml_body = file("./kubernetes/manifests/certificates/certificate-grafana.yaml")
-# depends_on = [kubectl_manifest.cluster_issuer]
-# }
+resource "kubectl_manifest" "certificate_grafana" {
+    yaml_body = file("./kubernetes/manifests/certificates/certificate-grafana.yaml")
+depends_on = [kubectl_manifest.cluster_issuer]
+}
 
-# resource "kubectl_manifest" "certificate_prometheus" {
-#     yaml_body = file("./kubernetes/manifests/certificates/certificate-prometheus.yaml")
-# depends_on = [kubectl_manifest.cluster_issuer]
-# }
+resource "kubectl_manifest" "certificate_prometheus" {
+    yaml_body = file("./kubernetes/manifests/certificates/certificate-prometheus.yaml")
+depends_on = [kubectl_manifest.cluster_issuer]
+}
 
-# resource "kubectl_manifest" "certificate_wildcards" {
-#     yaml_body = file("./kubernetes/manifests/certificates/certificate-wildcards.yaml")
-# depends_on = [kubectl_manifest.cluster_issuer]
-# }
+resource "kubectl_manifest" "certificate_wildcards" {
+    yaml_body = file("./kubernetes/manifests/certificates/certificate-wildcards.yaml")
+depends_on = [kubectl_manifest.cluster_issuer]
+}
 
-# resource "kubectl_manifest" "ingress" {
-#     yaml_body = file("./kubernetes/manifests/ingress.yaml")
-#     depends_on = [kubectl_manifest.cluster_issuer]
-# }
+resource "kubectl_manifest" "ingress" {
+    yaml_body = file("./kubernetes/manifests/ingress.yaml")
+    depends_on = [kubectl_manifest.cluster_issuer]
+}
 
